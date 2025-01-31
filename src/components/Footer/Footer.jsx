4ddb0fc2e4cf8c431726ef363719328ee1a3b2b2
MukitHossen7 <hossenmukit7@gmail.com>
2025-01-31 22:34:18 +0600

    Update Footer component text to display copyright notice

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div className="text-4xl font-bold text-center">This is Footer</div>;
+  return <div className="text-4xl font-bold text-center">@copy right 2028</div>;
 };
 
 export default Footer;
